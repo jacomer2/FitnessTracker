ea72b8305a9c933bd7e354e32fe6f5bf87ed55f7
jacomer2 <69131100+jacomer2@users.noreply.github.com>
2024-04-09 14:06:35 -0400

    first commit

---
+++ FitnessTracker/src/main/java/com/CS440/FitnessTracker/FitnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.CS440.FitnessTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessTrackerApplication.class, args);
+	}
+
+}
---
+++ FitnessTracker/src/test/java/com/CS440/FitnessTracker/FitnessTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CS440.FitnessTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
