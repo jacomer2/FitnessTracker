ab9c53c2857480ba76aa38d25d2195600557f594
jacomer2 <69131100+jacomer2@users.noreply.github.com>
2024-04-23 15:57:34 -0400

    updated dbmanger test

--- src/test/java/com/CS440/FitnessTracker/DatabaseManagerTest/DatabaseManagerTest.java
+++ src/test/java/com/CS440/FitnessTracker/DatabaseManagerTest/DatabaseManagerTest.java
@@ -1,23 +1,23 @@
 package com.CS440.FitnessTracker.DatabaseManagerTest;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.sql.SQLException;
+import javax.sql.DataSource;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.CS440.FitnessTracker.Database.DatabaseManager;
 
 @SpringBootTest
 public class DatabaseManagerTest {      
     
-    DatabaseManager dbms = new DatabaseManager();
+    @Autowired
+    private DataSource dataSource;
 
     @Test
-    public void databaseTest() throws SQLException
-    {
-        assertNotNull(dbms.connect());
+    public void testConnection() {
+        System.out.println("\n\n***Test Connection***\n\n");
 
+        assertNotNull(dataSource, "DataSource is null");
     }
 }
 
